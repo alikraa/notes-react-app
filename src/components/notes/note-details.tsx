ff34dc559870133e8f188cd55571f6edf75c7f98
Alina <krakhmaalina@gmail.com>
2024-03-12 23:49:13 +0300

    note details component

---
+++ src/components/notes/note-details.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { FaPen } from 'react-icons/fa6';
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { ModalWindow } from '../modal-window/modal-window.tsx';
+import { ModalFormNote } from '../modal-form-note/modal-form-note.tsx';
+import { noteTemplate } from '../../ts/notes-data.ts';
+import { findNote } from '../../ts/notes-actions.ts';
+import { useAppSelector } from '../../store/hooks.ts';
+import { RootState } from '../../store/store.ts';
+import './notes.scss';
+
+export function NoteDetails() {
+  const { notes, currentNoteId } = useAppSelector(
+    (state: RootState) => state.notesData
+  );
+  const navigate = useNavigate();
+
+  const [note, setNote] = useState(noteTemplate);
+  const [openForm, setOpenForm] = useState(true);
+
+  useEffect(() => {
+    if (currentNoteId) {
+      const currentNote = findNote(currentNoteId, notes);
+      const isNote = currentNote ?? noteTemplate;
+      setNote(isNote);
+    }
+  }, [currentNoteId, notes]);
+
+  useEffect(() => {
+    const currentNote = findNote(currentNoteId, notes);
+    if (currentNote === undefined) {
+      navigate('/');
+    }
+  }, [currentNoteId, navigate, notes]);
+
+  const date = `${new Date(note.noteDate)
+    .toLocaleTimeString()
+    .slice(0, 5)}, ${new Date(note.noteDate).toLocaleDateString()}`;
+
+  const openModalForm = () => {
+    setOpenForm(!openForm);
+  };
+
+  return (
+    <div className="notes-note-details">
+      <div className="notes-note-details__actions">
+        <button
+          type="button"
+          aria-label="Back"
+          className="notes-note-details__back-button"
+          onClick={() => navigate(-1)}
+        >
+          <IconContext.Provider
+            value={{ className: 'arrow-icon', size: '3em' }}
+          >
+            <MdKeyboardBackspace />
+          </IconContext.Provider>
+        </button>
+
+        <button
+          className="notes-note-details__edit-button"
+          type="button"
+          aria-label="Edit Note"
+          onClick={openModalForm}
+        >
+          <IconContext.Provider
+            value={{ className: 'pencil-icon', size: '1.7em' }}
+          >
+            <FaPen />
+          </IconContext.Provider>
+        </button>
+      </div>
+
+      <div className="notes-note-details__note">
+        <h2 className="notes-note-details__note__name">{note.noteName}</h2>
+        <p className="notes-note-details__note__date">{date}</p>
+        <div className="notes-note-details__note__text">{note.noteText}</div>
+      </div>
+
+      <ModalWindow hidden={openForm} handleClick={openModalForm}>
+        <ModalFormNote handleClick={openModalForm} />
+      </ModalWindow>
+    </div>
+  );
+}
