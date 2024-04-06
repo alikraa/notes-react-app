dc1dd037944c36b0ce5304127d53131ea55b41e9
Alina <krakhmaalina@gmail.com>
2024-04-06 23:53:24 +0300

    empty list label

--- src/components/notes/notes-list.tsx
+++ src/components/notes/notes-list.tsx
@@ -6,8 +6,9 @@ import { ModalWindowAction } from '../../ts/types.ts';
 import './notes.scss';
 
 export function NotesList({ handleClick }: ModalWindowAction) {
-  const { notes, favoriteNotes, isFavorite, searchValue, error } =
-    useAppSelector((state: RootState) => state.notesData);
+  const { notes, favoriteNotes, isFavorite, searchValue } = useAppSelector(
+    (state: RootState) => state.notesData
+  );
 
   const [currentList, setCurrentList] = useState(notes);
 
@@ -18,16 +19,19 @@ export function NotesList({ handleClick }: ModalWindowAction) {
 
   return (
     <div className="notes-list">
-      {error !== null && <h2>Что-то пошло не так: {String(error)}</h2>}
-      {currentList
-        .filter(
-          (item) =>
-            item.noteName.toLowerCase().includes(searchValue) ||
-            item.noteText.toLowerCase().includes(searchValue)
-        )
-        .map((item) => (
-          <Note key={item.noteId} note={item} handleClick={handleClick} />
-        ))}
+      {currentList.length !== 0 ? (
+        currentList
+          .filter(
+            (item) =>
+              item.noteName.toLowerCase().includes(searchValue) ||
+              item.noteText.toLowerCase().includes(searchValue)
+          )
+          .map((item) => (
+            <Note key={item.noteId} note={item} handleClick={handleClick} />
+          ))
+      ) : (
+        <h2 className="message">Пусто :(</h2>
+      )}
     </div>
   );
 }
